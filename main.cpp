f5154c6aafe2759ae95304bcdf2e581d7f6c6364
Никифоров Артем, ИП-516 <nikiforovartem05@gmail.com>
2025-10-17 15:07:40 +0000

    Исправлен отступ

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main(int argc, char** argv) {
  char* str = (char*)malloc(sizeof(char) * SIZE);
  printf("Введите своё имя:\n");
  scanf("%s", str);
- printf("Привет, %s", str);
+        printf("Привет, %s", str);
  return 0;
 }
